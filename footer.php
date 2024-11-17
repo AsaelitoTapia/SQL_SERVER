52c6869693c3a0ec0112f9df10b5784433be29e3
Asael Tapia <asaeltapia999@gmail.com>
2024-11-17 08:34:00 -0600

    Codigo de diseño para el pie de pagina

---
+++ footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Iconos de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1AnmseVgNHz7JB6VR8x0L1nPuDfuRAgp9C3tKnzV7hz6W+wxted" crossorigin="anonymous">
+    <title>Pie de Página Mejorado</title>
+</head>
+<body>
+
+<!-- Pie de página -->
+<footer class="container-fluid bg-dark text-light mt-5">
+    <div class="row">
+        <!-- Contenido del pie de página -->
+        <div class="col-md text-center py-3">
+            <h5 class="mb-3">Librería Asael</h5>
+            <p class="mb-0">© 2024 Librería Asaelito Tapia. Todos los derechos reservados.</p>
+            <!-- Enlaces a redes sociales -->
+            <div class="mt-2">
+                <a href="#" class="text-light mx-2" style="text-decoration: none;">
+                    <i class="bi bi-facebook"></i> Facebook
+                </a>
+                <a href="#" class="text-light mx-2" style="text-decoration: none;">
+                    <i class="bi bi-twitter"></i> Twitter
+                </a>
+                <a href="#" class="text-light mx-2" style="text-decoration: none;">
+                    <i class="bi bi-instagram"></i> Instagram
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap JavaScript y dependencias de Popper -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
