0a196156dda36dcff41141528d08464877a95d58
Asael Tapia <asaeltapia999@gmail.com>
2024-11-17 08:36:18 -0600

    Codigo de diseño para el encabezado

---
+++ header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Base de datos/Libreria</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  </head>
+  <body>
+      <!-- Encabezado -->
+      <div class="container-fluid bg-dark">
+          <div class="row">
+              <div class="col-md">
+                  <header class="py-4">
+                      <!-- Título principal -->
+                      <h1 class="text-center text-white font-weight-bold">Libreria con Bootstrap 5</h1>
+                      <!-- Subtítulo con icono -->
+                      <p class="text-center text-white"><i class="bi bi-file-earmark-text"></i> Diseño de Librería</p>
+                  </header>
+              </div>
+          </div>
+      </div>
+      <!-- Bootstrap JavaScript Libraries -->
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
